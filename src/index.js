454618ac3be5e441bd1805be0d4b003886b8fb14
Cordozar <cant09y@gmail.com>
2022-06-23 22:03:22 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let n_abs = String(Math.abs(n));
+    let res = "";
+    for (let i = 0; i < n_abs.length; i = i + 1) {
+        res = res + n_abs[n_abs.length - 1 - i];
+    }
+    return res;
+};
